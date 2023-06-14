9bc98df546f6f9a593dd6d9237395e449638956f
koen-benne <koen.benne@gmail.com>
2023-06-14 13:57:40 +0200

    styling modal

--- content.js
+++ content.js
@@ -1,17 +1,17 @@
 const modalHTML = `
-<div class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog">
-  <div class="modal-content">
-    <div class="modal-header">
-      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="closeModal"></button>
+<div class="ssm-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="ssm-modal-dialog">
+  <div class="ssm-modal-content">
+    <div class="ssm-modal-header">
+      <h5 class="ssm-modal-title" >Modal title</h5>
     </div>
-    <div class="modal-body">
+    <div class="ssm-modal-body">
       ...
     </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      <button type="button" class="btn btn-primary">Save changes</button>
+    <div class="ssm-modal-footer">
+      <button type="button" class="ssm-btn ssm-btn-secondary" data-bs-dismiss="modal" onclick="document.getElementById('ssm-parent').style.display ='none';
+">Close</button>
+      <button type="button" class="ssm-btn ssm-btn-primary">Save changes</button>
     </div>
   </div>
 </div>
@@ -19,14 +19,14 @@ const modalHTML = `
 `;
 
 function closeModal() {
-  document.getElementById("surfsafe-modal").style.display ="none";
+  document.getElementById("ssm-parent").style.display = "none";
 }
 
 window.addEventListener("DOMContentLoaded", function () {
 
   // Create a new popup element
   const popup = document.createElement("div");
-  popup.id = "surfsafe-modal";
+  popup.id = "ssm-parent";
   popup.innerHTML = modalHTML;
   document.body.appendChild(popup);
 });
